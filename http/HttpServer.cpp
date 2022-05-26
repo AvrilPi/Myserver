c755ec52e7e5f3457f1dfea8ba8c17a46ab93e83
AvrilPi <p183876@163.com>
2022-05-26 22:52:00 +0800

    add http

---
+++ http/HttpServer.cpp
@@ -0,0 +1,39 @@
+#include "HttpServer.h"
+#include "HttpParse.h"
+#include "HttpResponse.h"
+#include "HttpRequest.h"
+#include "TcpClient.h"
+#include "Logger.h"
+#include "HttpParse.h"
+#include "Buffer.h"
+
+#include <gflags/gflags.h>
+
+DEFINE_int32(threadNum,4, "set thread num");
+
+using namespace std;
+ 
+void HttpServer::ReadCallback(Pine::clientPtr client,Buffer* inputBuffer){
+    string str = inputBuffer->getAllString();
+    LOG_INFO("%s",str.c_str());
+    unique_ptr<HttpInfo>& httpInfo = client->getHttpInfo();
+    httpInfo->reset();
+
+    httpInfo->request_->analyseFile(str,httpInfo);
+
+    // 设置response头文件并且发送请求的文件
+    httpInfo->response_->SendFile(client.get(),httpInfo);
+
+}
+
+void HttpServer::run(){
+    tcpServer_.setThreadNum(FLAGS_threadNum);
+    LOG_INFO("conf threadNum:%d",FLAGS_threadNum);
+    tcpServer_.setClientReadCallback(std::bind(&HttpServer::ReadCallback,this,std::placeholders::_1,std::placeholders::_2));
+    tcpServer_.start();
+    loop_.loop();
+}
+
+HttpServer::~HttpServer() = default;
+HttpServer::HttpServer():loop_(),tcpServer_(&loop_),postCallback_(){
+}
---
+++ http/HttpServer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <functional>
+#include <memory>
+#include "TcpClientCallback.h"
+#include "TcpServer.h"
+#include "EventLoop.h"
+#include "HttpCallback.h"
+
+class TcpClient;
+class Buffer;
+class HttpInfo;
+
+
+class HttpServer{
+private:
+    using callback = std::function<bool(std::string,std::string)>;
+private:
+    EventLoop loop_;// 主loop
+    TcpServer tcpServer_;
+    callback postCallback_;
+public:
+    HttpServer();
+    // 用户自定义的回调函数要正确的处理异常和自己负责关闭套接字
+    void ReadCallback(Pine::clientPtr t,Buffer* inputBuffer);
+    void run();
+    void setPostReadCallback(callback cb){
+        HttpCallback::setPostCB(cb);
+    }
+    ~HttpServer();
+};
